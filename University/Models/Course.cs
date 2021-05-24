b9c704a9a4fb9ba2b27de68856a5fa334c23e7a0
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-24 09:56:24 -0700

    add updated Program and Startup
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>
    Co-Authored-By: Cassandra Copp <cass16180@gmail.com>

---
+++ University/Models/Course.cs
@@ -0,0 +1,7 @@
+namespace University.Models
+{
+  public class Course
+  {
+    
+  }
+}
---
+++ University/Models/UniversityContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace University.Models
+{
+  public class UniversityContext : DbContext
+  {
+
+  }
+}
