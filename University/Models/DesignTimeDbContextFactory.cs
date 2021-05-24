3960be4a6994828d70c973a39dc9aee326cd48e3
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-24 11:16:40 -0700

    add factory/migration
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>
    Co-Authored-By: Cassandra Copp <cass16180@gmail.com>

---
+++ University/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace University.Models
+{
+  public class UniversityContextFactory : IDesignTimeDbContextFactory<UniversityContext>
+  {
+
+    UniversityContext IDesignTimeDbContextFactory<UniversityContext>.CreateDbContext(string[] args)
+    {
+      IConfiguration configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+        var builder = new DbContextOptionsBuilder<UniversityContext>();
+
+        builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["connectionStrings:DefaultConnection"]));
+
+        return new UniversityContext(builder.Options);
+    }
+  }
+}
